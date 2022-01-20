d9c40f118be04d467435ddd47dfce5e8db4bc338
yanmasa <ueda3ban@gmail.com>
2022-01-20 05:28:59 +0000

    基本データ型と参照型

---
+++ 09data.java
@@ -0,0 +1,21 @@
+class データ型 {
+    public static void main(String args[]){
+        // int x = 10;
+        // int y = x;
+        // y = 5;
+        // System.out.println(x);
+        // System.out.println(y);
+        
+        // int[] a = {3, 5, 7};
+        // int[] b = a;
+        // b[1] = 8 ;
+        // System.out.println(a[1]);
+        // System.out.println(b[1]);        
+        
+        String s = "hello";
+        String t = s;
+        t = "world" ;
+        System.out.println(s);
+        System.out.println(t);        
+    }
+}
